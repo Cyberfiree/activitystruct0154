c9bf1c28ca98c3806c4e269840d08f78c6cee17f
Cyberfire <buayagoreng99@gmail.com>
2024-04-02 11:41:04 +0800

    menambahkan // command pada project structure 1

--- Structure/Structure.cpp
+++ Structure/Structure.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-struct Mahasiswa {
+struct Mahasiswa { //Mendefinisikan sebuah struct untuk Mahasiswa 
 	string nim;
 	string nama;
 	string alamat;
@@ -10,19 +10,19 @@ struct Mahasiswa {
 
 int main()
 {
-	Mahasiswa mhs;
-	cout << "Nomor Mahasiswa : ";
-	cin >> mhs.nim;
-	cout << "Nama Mahasiswa : ";
-	cin >> mhs.nama;
-	cout << "Alamat Mahasiswa :";
-	cin >> mhs.alamat;
-	cout << "Umur Mahasiswa :";
-	cin >> mhs.umur;
+	Mahasiswa mhs; //Mendeklarasikan variabel struktur Mahasiswa
+	cout << "Nomor Mahasiswa : ";  // Menampilkan pesan untuk meminta nomor mahasiswa
+	cin >> mhs.nim; // Meminta input nomor mahasiswa dari pengguna dan menyimpannya di variabel nim
+	cout << "Nama Mahasiswa : "; // Menampilkan pesan untuk meminta nama mahasiswa
+	cin >> mhs.nama; // Meminta input nama mahasiswa dari pengguna dan menyimpannya di variabel nama
+	cout << "Alamat Mahasiswa :"; // Menampilkan pesan untuk meminta alamat mahasiswa
+	cin >> mhs.alamat; // Meminta input alamat mahasiswa dari pengguna dan menyimpannya di variabel alamat
+	cout << "Umur Mahasiswa :"; // Menampilkan pesan untuk meminta umur mahasiswa
+	cin >> mhs.umur; // Meminta input umur mahasiswa dari pengguna dan menyimpannya di variabel umur
 
-	cout << endl;
-	cout << "\n NIM : " << mhs.nim;
-	cout << "\n Nama : " << mhs.nama;
-	cout << "\n Alamat : " << mhs.alamat;
-	cout << "\n Umur : " << mhs.umur;
+	cout << endl; // Menampilkan baris kosong
+	cout << "\n NIM : " << mhs.nim; // Menampilkan nomor mahasiswa yang telah dimasukkan
+	cout << "\n Nama : " << mhs.nama; // Menampilkan nama mahasiswa yang telah dimasukkan
+	cout << "\n Alamat : " << mhs.alamat; // Menampilkan alamat mahasiswa yang telah dimasukkan
+	cout << "\n Umur : " << mhs.umur; // Menampilkan umur mahasiswa yang telah dimasukkan
 }
