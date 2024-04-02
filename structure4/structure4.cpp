860a7ab0e2738fef7d86c813ce7dd5a35bb86c73
Cyberfire <buayagoreng99@gmail.com>
2024-04-02 12:53:44 +0800

    mendefinisikan anggota desa dan kota berjenis string

--- structure4/structure4.cpp
+++ structure4/structure4.cpp
@@ -3,6 +3,8 @@
 using namespace std; // Menggunakan namespace std untuk menggunakan fungsi-fungsi dari standar C++
 
 struct DetailAlamat { // Mendefinisikan struktur DetailAlamat
+	string desa; // Mendefinisikan anggota desa berjenis string
+	string kota; // Mendefinisikan anggota kota berjenis string
 };
 
 int main()
