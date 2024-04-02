d826af715ea6d6381d727f341f70c3f3126419fd
Cyberfire <buayagoreng99@gmail.com>
2024-04-02 13:06:53 +0800

    input data setiap mahasiswa

--- structure4/structure4.cpp
+++ structure4/structure4.cpp
@@ -17,4 +17,27 @@ struct mahasiswa { // Mendefinisikan struktur mahasiswa
 int main() // Fungsi utama program
 {
 	mahasiswa mhs[3]; // Mendeklarasikan array mhs yang berisi 3 objek mahasiswa
+
+
+    // Input data untuk setiap mahasiswa
+    for (int i = 0; i < 3; i++) { // Mulai loop untuk setiap objek mahasiswa
+        cout << "Data ke-" << (i + 1) << ":" << endl; // Menampilkan pesan untuk data ke-i
+        cout << "Nomor mahasiswa: "; // Menampilkan pesan untuk nomor mahasiswa
+        getline(cin, mhs[i].nim); // Mendapatkan nomor mahasiswa dari input user
+
+        cout << "Nama Mahasiswa: "; // Menampilkan pesan untuk nama mahasiswa
+        getline(cin, mhs[i].nama); // Mendapatkan nama mahasiswa dari input user
+
+        cout << "Alamat Mahasiswa:" << endl; // Menampilkan pesan untuk alamat mahasiswa
+        cout << "\tNama Desa: "; // Menampilkan pesan untuk nama desa
+        cin >> mhs[i].alamat.desa; // Mendapatkan nama desa dari input user
+
+        cout << "\tNama Kota: "; // Menampilkan pesan untuk nama kota
+        cin >> mhs[i].alamat.kota; // Mendapatkan nama kota dari input user
+
+        cout << "Umur Mahasiswa: "; // Menampilkan pesan untuk umur mahasiswa
+        cin >> mhs[i].umur; // Mendapatkan umur mahasiswa dari input user
+        cin.ignore(1, '\n'); // Membersihkan karakter newline dari buffer input
+        cout << endl; // Mencetak baris kosong untuk memisahkan setiap input mahasiswa
+    }
 }
