240b479334b83dff7e2f2ed630b508837c2843a2
Cyberfire <buayagoreng99@gmail.com>
2024-04-02 12:53:20 +0800

    mendefinisikan struktur detailalamat

---
+++ structure4/structure4.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Menyertakan library input-output standar C++
+#include <string>
+using namespace std; // Menggunakan namespace std untuk menggunakan fungsi-fungsi dari standar C++
+
+struct DetailAlamat { // Mendefinisikan struktur DetailAlamat
+};
+
+int main()
+{
+}
